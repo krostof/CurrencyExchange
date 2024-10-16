676aa3590f9aafe38d5bc5a83cbf55702d9ec445
krostof <krostof4@wp.pl>
2024-10-16 22:53:22 +0200

    ref

--- src/main/java/org/example/currencyexchange/CurrencyExchangeController.java
+++ src/main/java/org/example/currencyexchange/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package org.example.currencyexchange;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,20 +16,23 @@ public class CurrencyExchangeController {
     public static final String MESSAGE_IF_AMOUNT_EQUAL_OR_LESS_THAN_ZERO = "Amount must be greater than zero.";
     private final NbpCurrFetcher nbpCurrFetcher;
 
-    @GetMapping("/")
-    public ResponseEntity<ResponseDto> currencyExchange(@RequestParam BigDecimal amount,
-                                                        @RequestParam String targetCurrency,
-                                                        @RequestParam String currency) {
+    @GetMapping("/currency-exchange")
+    public ResponseEntity<ResponseDto> getCurrencyExchange(@RequestParam BigDecimal amount,
+                                                           @RequestParam String sourceCurrency,
+                                                           @RequestParam String targetCurrency) {
         if (amount.compareTo(BigDecimal.ZERO) <= 0) {
             return ResponseEntity.badRequest().body(new ResponseDto(MESSAGE_IF_AMOUNT_EQUAL_OR_LESS_THAN_ZERO));
         }
 
         try {
-            String result = nbpCurrFetcher.currExchange(amount, currency, targetCurrency);
-            ResponseDto responseDto = new ResponseDto(result);
-            return ResponseEntity.ok(responseDto);
+            String result = nbpCurrFetcher.currExchange(amount, sourceCurrency, targetCurrency);
+            return ResponseEntity.ok(new ResponseDto(result));
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(new ResponseDto(e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDto("Unexpected error occurred: " + e.getMessage()));
         }
     }
+
 }
